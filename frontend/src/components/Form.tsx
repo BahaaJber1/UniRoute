6f81d55562c367b9c5d73bdd53d11cb1ffae8c0d
BahaaJber1 <bahaajber23@gmail.com>
2025-09-07 20:13:03 +0300

    Implement the zod logic for the form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,18 +1,25 @@
 import { useForm } from "react-hook-form";
 import ErrorMessage from "./ErrorMessage";
-import Button from "./LinkButton";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema, formFields } from "../types";
+import ButtonOrLink from "./ButtonOrLink";
 
 //The form accept 2 props the mode which can be login or signup  with default value of login to make it reusable
 //The other is the callback function that will fire up when the form is submitted, right now its only for log this data
 //to the concole as a default behavior, later you can update it to interact with the database.
 
-function Form({ mode = "login", onSubmit = (data) => console.log(data) }) {
+function Form({
+	mode = "login",
+	onSubmit = (data: formFields) => console.log(data),
+}) {
 	const {
 		register,
 		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
+		setError,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		resolver: zodResolver(formSchema),
+	});
 
 	return (
 		<>
@@ -20,43 +27,15 @@ function Form({ mode = "login", onSubmit = (data) => console.log(data) }) {
 				{mode !== "login" && (
 					<div>
 						<label htmlFor="firstName">First name</label>
-						<input
-							{...register("firstName", {
-								required: "First name is required",
-								minLength: {
-									value: 4,
-									message: "First name must be at least 4 characters",
-								},
-								pattern: {
-									value: /^[a-zA-Z]+$/,
-									message: "First name must contain only letters",
-								},
-							})}
-							type="text"
-							id="firstName"
-							name="firstName"
-						/>
+						<input {...register("firstName")} type="text" id="firstName" />
+
 						{errors.firstName?.message && (
 							<ErrorMessage>{errors.firstName?.message}</ErrorMessage>
 						)}
 
 						<label htmlFor="lastName">Last name</label>
-						<input
-							{...register("lastName", {
-								required: "Last name is required",
-								minLength: {
-									value: 4,
-									message: "Last name must be at least 4 characters",
-								},
-								pattern: {
-									value: /^[a-zA-Z]+$/,
-									message: "Last name must contain only letters",
-								},
-							})}
-							type="text"
-							id="lastName"
-							name="lastName"
-						/>
+						<input {...register("lastName")} type="text" id="lastName" />
+
 						{errors.lastName?.message && (
 							<ErrorMessage>{errors.lastName.message}</ErrorMessage>
 						)}
@@ -65,21 +44,11 @@ function Form({ mode = "login", onSubmit = (data) => console.log(data) }) {
 				<div>
 					<label htmlFor="studentNumber">Student number</label>
 					<input
-						{...register("studentNumber", {
-							required: "Student number is required",
-							minLength: {
-								value: 12,
-								message: "Student number must be at least 12 characters",
-							},
-							pattern: {
-								value: /^[0-9]{12}$/,
-								message: "Student number must contain only numbers",
-							},
-						})}
+						{...register("studentNumber")}
 						id="studentNumber"
 						type="text"
-						name="studentNumber"
 					/>
+
 					{errors.studentNumber?.message && (
 						<ErrorMessage>{errors.studentNumber?.message}</ErrorMessage>
 					)}
@@ -93,63 +62,26 @@ function Form({ mode = "login", onSubmit = (data) => console.log(data) }) {
 								type="radio"
 								value="male"
 								id="male"
-								name="gender"
-								{...register("gender", { required: "Gender is required" })}
+								{...register("gender")}
 							/>
 							<label htmlFor="male">Male</label>
 							<input
 								type="radio"
 								value="female"
 								id="female"
-								name="gender"
-								{...register("gender", { required: "Gender is required" })}
+								{...register("gender")}
 							/>
 							<label htmlFor="female">Female</label>
 							{errors.gender && (
 								<ErrorMessage>{errors.gender.message}</ErrorMessage>
 							)}
 						</fieldset>
-
-						<div>
-							<label htmlFor="email">E-mail</label>
-							<input
-								{...register("email", {
-									required: "Email is required",
-									minLength: {
-										value: 12,
-										message: "Email must be at least 12 characters",
-									},
-									pattern: {
-										value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/,
-										message: "Last name contain only letters",
-									},
-								})}
-								type="email"
-								id="email"
-								name="email"
-								autoComplete="email"
-							/>
-							{errors.email?.message && (
-								<ErrorMessage>{errors.email.message}</ErrorMessage>
-							)}
-						</div>
 					</>
 				)}
 
 				<div>
 					<label htmlFor="password">Password</label>
-					<input
-						{...register("password", {
-							required: "Password is required",
-							minLength: {
-								value: 8,
-								message: "Password must be at least 8 characters",
-							},
-						})}
-						type="password"
-						id="password"
-						name="password"
-					/>
+					<input {...register("password")} type="password" id="password" />
 
 					{errors.password?.message && (
 						<ErrorMessage>{errors.password?.message}</ErrorMessage>
@@ -159,26 +91,26 @@ function Form({ mode = "login", onSubmit = (data) => console.log(data) }) {
 						<>
 							<label htmlFor="confirmPassword">Confirm password</label>
 							<input
-								{...register("confirmPassword", {
-									required: "Confirm password is required",
-									minLength: {
-										value: 8,
-										message: "Confirm password must be at least 8 characters",
-									},
-									validate: (value) =>
-										value === watch("password") || "Passwords do not match",
-								})}
+								{...register("confirmPassword")}
 								type="password"
 								id="confirmPassword"
-								name="confirmPassword"
 							/>
+
 							{errors.confirmPassword?.message && (
 								<ErrorMessage>{errors.confirmPassword?.message}</ErrorMessage>
 							)}
 						</>
 					)}
 				</div>
-				{mode !== "login" ? <Button>Create</Button> : <Button>Login</Button>}
+				{mode !== "login" ? (
+					<ButtonOrLink disabled={isSubmitting}>
+						{isSubmitting ? "Loading..." : "Create"}
+					</ButtonOrLink>
+				) : (
+					<button disabled={isSubmitting}>
+						{isSubmitting ? "Loading..." : "Login"}
+					</button>
+				)}
 			</form>
 
 			{mode !== "login" ? (
